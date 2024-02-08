21361466bc1e6228386440daa100da7113556b3a
Verônica Vilas Boas <135287830+VeronicaVilas@users.noreply.github.com>
2024-02-08 09:57:01 -0300

    feat: Adicionado atividade concluida referente a semana 02

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,8 @@ import Home1 from '../views/pessoa1/Home.vue'
 import Home2 from '../views/pessoa2/Home.vue'
 import Home3 from '../views/pessoa3/Home.vue'
 import Home4 from '../views/pessoa4/Home.vue'
+import Pets2 from '../views/pessoa2/Pets.vue'
+import PetsProfilePessoa2 from '../views/pessoa2/Profile.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,7 +22,7 @@ const router = createRouter({
       component: Home1
     },
     {
-      path: '/home2',
+      path: '/adocoes-de-animais',
       name: 'home2',
       component: Home2
     },
@@ -33,6 +35,16 @@ const router = createRouter({
       path: '/home4',
       name: 'home4',
       component: Home4
+    },
+    {
+      path: '/lista-de-adocoes',
+      name: 'pets2',
+      component: Pets2
+    },
+    {
+      path: '/pets-adocao/:id/perfil',
+      name: 'PetsProfilePessoa2',
+      component: PetsProfilePessoa2
     },
    
   ]
